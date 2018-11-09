070026893d8726b50cff054d50769cd9131fc4e5
yutaka takagaki <takagakiyutaka@takagaki-no-mba.local>
2018-11-10 01:10:24 +0900

    Initial Commit

---
+++ QRreader1109/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRreader1109
+//
+//  Created by yutaka takagaki on 2018/11/10.
+//  Copyright © 2018年 yutaka takagaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
